008fb140d827d28c5f76e2c344ed8c8171c5e0ea
AndreaSzoboszlai <szobandi@gmail.com>
2019-06-23 18:13:57 +0200

    compressing and decompressing works

--- SanityArchiver/Form1.Designer.cs
+++ SanityArchiver/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SanityArchiver
 {
-    partial class Form1
+    partial class SanityArchiver
     {
         /// <summary>
         /// Required designer variable.
@@ -31,11 +31,15 @@
             this.openFileBrowser = new System.Windows.Forms.OpenFileDialog();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.browse = new System.Windows.Forms.Button();
+            this.openSelected = new System.Windows.Forms.Button();
+            this.compressing = new System.Windows.Forms.Button();
+            this.editProps = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // openFileBrowser
             // 
             this.openFileBrowser.FileName = "openFileBrowser";
+            this.openFileBrowser.InitialDirectory = "C:\\";
             // 
             // textBox1
             // 
@@ -46,23 +50,55 @@
             // 
             // browse
             // 
-            this.browse.Location = new System.Drawing.Point(35, 42);
+            this.browse.Location = new System.Drawing.Point(84, 46);
             this.browse.Name = "browse";
-            this.browse.Size = new System.Drawing.Size(125, 31);
+            this.browse.Size = new System.Drawing.Size(156, 31);
             this.browse.TabIndex = 1;
             this.browse.Text = "Browse files";
             this.browse.UseVisualStyleBackColor = true;
             this.browse.Click += new System.EventHandler(this.Browse_Click);
             // 
-            // Form1
+            // openSelected
+            // 
+            this.openSelected.Location = new System.Drawing.Point(84, 94);
+            this.openSelected.Name = "openSelected";
+            this.openSelected.Size = new System.Drawing.Size(156, 31);
+            this.openSelected.TabIndex = 2;
+            this.openSelected.Text = "Open Selected File";
+            this.openSelected.UseVisualStyleBackColor = true;
+            this.openSelected.Click += new System.EventHandler(this.OpenSelected_Click);
+            // 
+            // compressing
+            // 
+            this.compressing.Location = new System.Drawing.Point(84, 143);
+            this.compressing.Name = "compressing";
+            this.compressing.Size = new System.Drawing.Size(156, 31);
+            this.compressing.TabIndex = 3;
+            this.compressing.Text = "Compressing File";
+            this.compressing.UseVisualStyleBackColor = true;
+            this.compressing.Click += new System.EventHandler(this.Compressing_Click);
+            // 
+            // editProps
+            // 
+            this.editProps.Location = new System.Drawing.Point(84, 190);
+            this.editProps.Name = "editProps";
+            this.editProps.Size = new System.Drawing.Size(156, 31);
+            this.editProps.TabIndex = 4;
+            this.editProps.Text = "Edit Properties";
+            this.editProps.UseVisualStyleBackColor = true;
+            // 
+            // SanityArchiver
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(337, 450);
+            this.Controls.Add(this.editProps);
+            this.Controls.Add(this.compressing);
+            this.Controls.Add(this.openSelected);
             this.Controls.Add(this.browse);
             this.Controls.Add(this.textBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "SanityArchiver";
+            this.Text = "Sanity Archiver";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -73,6 +109,9 @@
         private System.Windows.Forms.OpenFileDialog openFileBrowser;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.Button openSelected;
+        private System.Windows.Forms.Button compressing;
+        private System.Windows.Forms.Button editProps;
     }
 }
 
--- SanityArchiver/Form1.cs
+++ SanityArchiver/Form1.cs
@@ -8,12 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.IO.Compression;
 
 namespace SanityArchiver
 {
-    public partial class Form1 : Form
+    public partial class SanityArchiver : Form
     {
-        public Form1()
+        string selected;
+        FileInfo fileSelected;
+        public SanityArchiver()
         {
             InitializeComponent();
         }
@@ -23,11 +26,70 @@ namespace SanityArchiver
             DialogResult result = openFileBrowser.ShowDialog();
             if (result == DialogResult.OK)
             {
-                string selected = openFileBrowser.FileName;
+                selected = openFileBrowser.FileName;
                 textBox1.Text = selected;
-                System.Diagnostics.Process.Start(selected);
+                fileSelected = new FileInfo(selected);
+                if (fileSelected.Extension != ".gz")
+                {
+                    compressing.Text = "Compress File";
+                }
+                else
+                {
+                    compressing.Text = "Decompress File";
+                }
             }
-            Console.WriteLine(result);
+        }
+
+        private void OpenSelected_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(selected);
+        }
+
+        private void Compressing_Click(object sender, EventArgs e)
+        {
+            fileSelected = new FileInfo(selected);
+            if (fileSelected.Extension != ".gz")
+            { 
+                Compress();
+            }
+            else
+            {
+                DeCompress();
+            }
+
+        }
+
+        private void Compress()
+        {
+            FileInfo fileSelectedFrom = new FileInfo(selected);
+            FileInfo fileSelectedTo = new FileInfo(selected);
+            FileStream input = fileSelectedTo.OpenRead();
+            FileStream output = File.Create(fileSelectedFrom.FullName + ".gz");
+            GZipStream Compressor = new GZipStream(output, CompressionMode.Compress);
+            int b = input.ReadByte();
+            while (b != -1)
+            {
+                Compressor.WriteByte((byte)b);
+                b = input.ReadByte();
+            }
+            Compressor.Close();
+            input.Close();
+            output.Close();
+        }
+
+        private void DeCompress()
+        {
+            FileInfo fileSelectedTo = new FileInfo(selected.Remove(selected.Length - fileSelected.Extension.Length));
+            FileInfo fileSelectedFrom = new FileInfo(selected);
+            FileStream input = fileSelectedFrom.OpenRead();
+            FileStream output = File.Create(Path.GetDirectoryName(selected) +"\\" +  @"" + fileSelectedTo.Name);
+            GZipStream deCompressor = new GZipStream(input, CompressionMode.Decompress);
+
+            deCompressor.CopyTo(output);
+
+            deCompressor.Close();
+            input.Close();
+            output.Close();
         }
     }
 }
