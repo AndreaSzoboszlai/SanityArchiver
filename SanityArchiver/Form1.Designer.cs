95d2bbe7e64930cf9637f1ba71dabf2aef211db8
AndreaSzoboszlai <szobandi@gmail.com>
2019-06-23 15:14:18 +0200

    basic directory browsing form

---
+++ SanityArchiver/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace SanityArchiver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileBrowser = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.browse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileBrowser
+            // 
+            this.openFileBrowser.FileName = "openFileBrowser";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(305, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(35, 42);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(125, 31);
+            this.browse.TabIndex = 1;
+            this.browse.Text = "Browse files";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.browse);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileBrowser;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button browse;
+    }
+}
+
---
+++ SanityArchiver/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SanityArchiver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileBrowser.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string selected = openFileBrowser.FileName;
+                textBox1.Text = selected;
+                System.Diagnostics.Process.Start(selected);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
